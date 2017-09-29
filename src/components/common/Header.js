6803a357e948f66e4a33f4db6c254023403ac8eb
Paul Antonau <paul.antonau@gmail.com>
2017-09-30 01:58:12 +0300

    Improved the Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,10 +3,22 @@ import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Test</IndexLink>
-      {" | "}
-      <Link to="/test" activeClassName="active">Test</Link>
+    <nav className="navbar navbar-inverse">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <IndexLink className="navbar-brand" to="/">HIMS</IndexLink>
+        </div>
+        <ul className="nav navbar-nav">
+          <li className="active"><Link to="/test">Tests CRUD Grid</Link></li>
+        </ul>
+        <form className="navbar-form navbar-left">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="email" />
+            <input type="text" className="form-control" placeholder="password" />
+          </div>
+          <button type="submit" className="btn btn-default">Login</button>
+        </form>
+      </div>
     </nav>
   );
 };
