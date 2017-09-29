415099ce4c11aa3e7ad9dcdebf99cd9c13f18ba4
Paul Antonau <paul.antonau@gmail.com>
2017-09-30 01:57:27 +0300

    Added the new testActions

--- src/actions/testActions.js
+++ src/actions/testActions.js
@@ -16,3 +16,50 @@ export function loadTests() {
     });
   };
 }
+
+export function createTestSuccess(test) {
+  return {type: types.CREATE_TEST_SUCCESS, test};
+}
+
+export function updateTestSuccess(test) {
+  return {type: types.UPDATE_TEST_SUCCESS, test};
+}
+
+export function saveTest(test) {
+  return function (dispatch, getState) {
+      let saveProm = test.testId
+        ? testApi.updateTest(test)
+        : testApi.createTest(test);
+      let successCallback = test.testId
+        ? updateTestSuccess
+        : createTestSuccess;
+
+      return saveProm
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((test) => {
+        if (test.testId)
+          dispatch(successCallback(test));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
+
+export function deleteTestSuccess(testId) {
+  return {type: types.DELETE_TEST_SUCCESS, testId};
+}
+
+export function deleteTest(testId) {
+  return function (dispatch) {
+    return testApi.deleteTest(testId)
+    .then((resp) => {
+      if (resp.ok)
+        dispatch(deleteTestSuccess(testId));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
