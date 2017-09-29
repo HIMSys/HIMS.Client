f0043ed13ea42a0e3dded50e4d8051064e968dd4
Paul Antonau <paul.antonau@gmail.com>
2017-09-30 02:00:25 +0300

    Added the create and the delete api methods

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,19 +1,47 @@
 import {WEB_API_URL} from './webApi';
 
 const GET_ALL_TESTS = '/api/test/test';
+const ADD_TEST = '/api/test/addtest';
+const UPDATE_TEST = '/api/test/updatetest';
+const DELETE_TEST = '/api/test/deletetest';
 
 const myHeaders = new Headers();
 
-const myInit = { method: 'GET',
+const myGetInit = { method: 'GET',
                headers: myHeaders,
                mode: 'cors',
                cache: 'default' };
 
+const myPostInit = { method: "POST",
+                    headers: {'Content-Type': 'application/json'},
+                    mode: 'cors',
+                    cache: 'default'};
+
 class TestApi {
   static getAllTests() {
-    return fetch(WEB_API_URL + GET_ALL_TESTS, myInit);
+    return fetch(WEB_API_URL + GET_ALL_TESTS, myGetInit);
   }
 
+  static createTest(test) {
+    let post = Object.assign({}, myPostInit);
+    post.body = JSON.stringify(test);
+
+    return fetch(WEB_API_URL + ADD_TEST, post);
+  }
+
+  static updateTest(test) {
+    let post = Object.assign({}, myPostInit);
+    post.body = JSON.stringify(test);
+
+    return fetch(WEB_API_URL + UPDATE_TEST, post);
+  }
+
+  static deleteTest(testId) {
+    let post = Object.assign({}, myPostInit);
+    post.body = JSON.stringify(testId);
+
+    return fetch(WEB_API_URL + DELETE_TEST, post);
+  }
 }
 
 export default TestApi;
