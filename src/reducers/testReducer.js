44130c5e27d45fcb382f4c8d75b9a4b560e04b0c
Paul Antonau <paul.antonau@gmail.com>
2017-09-30 01:58:55 +0300

    Implemented the new testReducer

--- src/reducers/testReducer.js
+++ src/reducers/testReducer.js
@@ -1,11 +1,27 @@
 import * as types from '../actions/actionTypes';
+import initialState from "./initialState";
 
-export default  function courseReducer(state = [], action) {
+export default  function courseReducer(state = initialState.tests, action) {
  switch(action.type) {
    case types.LOAD_TESTS_SUCCESS:
      return action.tests;
 
-   default:
+   case types.CREATE_TEST_SUCCESS:
+     return [
+       ...state,
+       Object.assign({}, action.test)
+     ];
+
+   case types.UPDATE_TEST_SUCCESS:
+     return [
+       ...state.filter(test => test.testId !== action.test.testId),
+       Object.assign({}, action.test)
+     ];
+
+  case types.DELETE_TEST_SUCCESS:
+     return state.filter(test => test.testId !== action.testId);
+
+  default:
      return state;
  }
 }
